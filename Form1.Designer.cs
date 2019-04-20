6d1232a0b62cbe064b3e8386779d195a860cf2e5
unknown <datndd2604@gmail.com>
2019-04-20 21:42:16 +0700

    Upload file game flappy bird

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pcbOngTren1 = new System.Windows.Forms.PictureBox();
+            this.pcbOngDuoi1 = new System.Windows.Forms.PictureBox();
+            this.pcbOngTren2 = new System.Windows.Forms.PictureBox();
+            this.pcbOngDuoi2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pcbPlay = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.conchim = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOngTren1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOngDuoi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOngTren2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOngDuoi2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.conchim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbOngTren1
+            // 
+            this.pcbOngTren1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pcbOngTren1.BackgroundImage")));
+            this.pcbOngTren1.Location = new System.Drawing.Point(123, -770);
+            this.pcbOngTren1.Name = "pcbOngTren1";
+            this.pcbOngTren1.Size = new System.Drawing.Size(271, 1001);
+            this.pcbOngTren1.TabIndex = 0;
+            this.pcbOngTren1.TabStop = false;
+            this.pcbOngTren1.Click += new System.EventHandler(this.pcbOngTren1_Click);
+            // 
+            // pcbOngDuoi1
+            // 
+            this.pcbOngDuoi1.Image = ((System.Drawing.Image)(resources.GetObject("pcbOngDuoi1.Image")));
+            this.pcbOngDuoi1.Location = new System.Drawing.Point(123, 319);
+            this.pcbOngDuoi1.Name = "pcbOngDuoi1";
+            this.pcbOngDuoi1.Size = new System.Drawing.Size(279, 498);
+            this.pcbOngDuoi1.TabIndex = 1;
+            this.pcbOngDuoi1.TabStop = false;
+            // 
+            // pcbOngTren2
+            // 
+            this.pcbOngTren2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pcbOngTren2.BackgroundImage")));
+            this.pcbOngTren2.Location = new System.Drawing.Point(582, -770);
+            this.pcbOngTren2.Name = "pcbOngTren2";
+            this.pcbOngTren2.Size = new System.Drawing.Size(271, 1001);
+            this.pcbOngTren2.TabIndex = 2;
+            this.pcbOngTren2.TabStop = false;
+            // 
+            // pcbOngDuoi2
+            // 
+            this.pcbOngDuoi2.Image = ((System.Drawing.Image)(resources.GetObject("pcbOngDuoi2.Image")));
+            this.pcbOngDuoi2.Location = new System.Drawing.Point(582, 302);
+            this.pcbOngDuoi2.Name = "pcbOngDuoi2";
+            this.pcbOngDuoi2.Size = new System.Drawing.Size(279, 498);
+            this.pcbOngDuoi2.TabIndex = 3;
+            this.pcbOngDuoi2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pcbPlay
+            // 
+            this.pcbPlay.BackColor = System.Drawing.Color.Transparent;
+            this.pcbPlay.BackgroundImage = global::FlappyBird.Properties.Resources.nut_play_removebg;
+            this.pcbPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pcbPlay.Location = new System.Drawing.Point(12, 12);
+            this.pcbPlay.Name = "pcbPlay";
+            this.pcbPlay.Size = new System.Drawing.Size(50, 50);
+            this.pcbPlay.TabIndex = 5;
+            this.pcbPlay.TabStop = false;
+            this.pcbPlay.Click += new System.EventHandler(this.pcbPlay_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(906, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Điểm: ";
+            // 
+            // conchim
+            // 
+            this.conchim.BackColor = System.Drawing.Color.Transparent;
+            this.conchim.BackgroundImage = global::FlappyBird.Properties.Resources.Chim_0nen;
+            this.conchim.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.conchim.Location = new System.Drawing.Point(170, 253);
+            this.conchim.Name = "conchim";
+            this.conchim.Size = new System.Drawing.Size(60, 40);
+            this.conchim.TabIndex = 7;
+            this.conchim.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1350, 664);
+            this.Controls.Add(this.conchim);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pcbPlay);
+            this.Controls.Add(this.pcbOngDuoi2);
+            this.Controls.Add(this.pcbOngTren2);
+            this.Controls.Add(this.pcbOngDuoi1);
+            this.Controls.Add(this.pcbOngTren1);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOngTren1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOngDuoi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOngTren2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbOngDuoi2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.conchim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbOngTren1;
+        private System.Windows.Forms.PictureBox pcbOngDuoi1;
+        private System.Windows.Forms.PictureBox pcbOngTren2;
+        private System.Windows.Forms.PictureBox pcbOngDuoi2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pcbPlay;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox conchim;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyBird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int x_ong1, x_ong2;
+
+        int diemso = 0;
+
+        int KhoangcachHaiOng = 200;
+        int x_ongtren1, y_ongtren1;
+        int x_ongduoi1,y_ongduoi1;
+        int x_conchim, y_conchim;
+       
+        int x_ongtren2,y_ongtren2;
+        int dem = 0;
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
+            if (y_conchim + conchim.Height <= this.Height)
+            {
+                y_conchim += 10;
+                conchim.Location = new Point(x_conchim, y_conchim);
+                if (x_conchim + conchim.Width >= x_ong1 && x_conchim + conchim.Width <= x_ong1 + pcbOngTren1.Width)
+                {
+                    if (y_conchim <= 1000 + y_ongtren1 || y_conchim+conchim.Height >= y_ongduoi1)
+                    {
+                        timer1.Stop();
+                        conchim.BackgroundImage = new Bitmap(@"D:\NodeJS\FlappyBird1\FlappyBird\FlappyBird\Picture\ChimChet.png");
+                        timer2.Stop();
+                        //music game over
+                        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+                        player.URL = "1.wav.mp3";
+                        player.controls.play();
+
+                        conchim.BackgroundImage = new Bitmap(@"C:\Users\asus\source\repos\FlappyBird\FlappyBird\Picture\ChimChet.png");
+                        if (MessageBox.Show("Điểm: " + diemso.ToString() + "\n" + "Bạn có muốn chơi lại không?",
+                            "Game Over!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                            Application.Exit();
+                        else
+                        {
+                            this.Hide();
+                            Form1 fr = new Form1();
+                            fr.Show();
+                        }
+
+                    }
+                }
+                if (x_conchim + conchim.Width >= x_ong2 && x_conchim + conchim.Width <= x_ong2 + pcbOngTren2.Width)
+                {
+                    if (y_conchim <= 1000 + y_ongtren2 || y_conchim + conchim.Height >= y_ongduoi2)
+                    {
+                        timer1.Stop();
+                        conchim.BackgroundImage = new Bitmap(@"D:\NodeJS\FlappyBird1\FlappyBird\FlappyBird\Picture\Chim-0nen.png");
+                        timer2.Stop();
+                        //music game over
+                        WMPLib.WindowsMediaPlayer player = new WMPLib.WindowsMediaPlayer();
+                        player.URL = "1.wav.mp3";
+                        player.controls.play();
+
+                        conchim.BackgroundImage = new Bitmap(@"C:\Users\asus\source\repos\FlappyBird\FlappyBird\Picture\ChimChet.png");
+                        if (MessageBox.Show("Điểm: " + diemso.ToString() + "\n" + "Bạn có muốn chơi lại không?",
+                            "Game Over!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No)
+                            Application.Exit();
+                        else
+                        {
+                            this.Hide();
+                            Form1 fr = new Form1();
+                            fr.Show();
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+                timer1.Start();
+
+               
+            }
+        }
+
+        private void pcbOngTren1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                y_conchim -= 50;
+              
+
+            }
+        }
+
+        private void pcbPlay_Click(object sender, EventArgs e)
+        {
+           
+            dem++;
+            if (dem % 2 != 0)
+            {
+                timer1.Start();
+                timer2.Start();
+            }
+            else
+            {
+                timer1.Stop();
+                timer2.Stop();
+            }
+           
+        }
+
+        int x_ongduoi2, y_ongduoi2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            Size sizeplay = new Size(50, 50);
+            pcbPlay.Size = sizeplay;
+            timer1.Interval = 1;
+            timer2.Interval = 50;
+            //timer1.Start();
+            //full screen 
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Left = 0;
+            this.Top = 0;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            x_ong1 =this.Width+ 300;
+            y_ongtren1 = -800;
+          
+            y_ongduoi1 = (1000 + y_ongtren1) + KhoangcachHaiOng;
+            //x_ong2 = x_ong1+ pcbOngTren1.Width + 400;
+            x_ong2 = x_ong1 + pcbOngTren1.Width + 400;
+          
+            y_ongtren2 = -800;
+
+           
+            y_ongduoi2 = (1000 + y_ongtren1) + KhoangcachHaiOng;
+            Size size = new Size(275, 1000);
+            pcbOngDuoi1.Size = size;
+            pcbOngTren1.Size = size;
+
+
+
+            pcbOngTren1.Location = new Point(x_ong1, y_ongtren1);
+            pcbOngDuoi1.Location = new Point(x_ong1, y_ongduoi1);
+            pcbOngTren2.Location = new Point(x_ong2, y_ongtren2);
+            pcbOngDuoi2.Location = new Point(x_ong2, y_ongduoi2);
+            //con chim
+            Size sizeChim = new Size(101,80);
+            conchim.Size = sizeChim;
+            x_conchim = conchim.Location.X;
+            y_conchim = conchim.Location.Y;
+
+           
+      
+          
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //ham chong rung
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | 
+                ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
+            x_ong1 -= 10;
+            x_ong2 -= 10;
+            int KhoangcachHaiOng = 200;
+
+            pcbOngTren1.Location = new Point(x_ong1, y_ongtren1);
+            pcbOngDuoi1.Location = new Point(x_ong1, y_ongduoi1);
+            pcbOngTren2.Location = new Point(x_ong2, y_ongtren2);
+            pcbOngDuoi2.Location = new Point(x_ong2, y_ongduoi2);
+
+            if (pcbOngTren1.Width + x_ong1 <= 0)
+            {
+                diemso++;
+                Random rd = new Random();
+                y_ongtren1 = rd.Next(-800, -700);
+                y_ongduoi1 = (1000 + y_ongtren1) + KhoangcachHaiOng;
+                x_ong1 = 500 + pcbOngTren2.Width + 400;
+                pcbOngTren1.Location = new Point(x_ong1, y_ongtren1);
+                pcbOngDuoi1.Location = new Point(x_ong1, y_ongduoi1);
+            }
+            if (x_ong2 + pcbOngTren2.Width <= 0)
+            {
+                diemso++;
+          
+                Random rd = new Random();
+                y_ongtren2 = rd.Next(-700, -400);
+                y_ongduoi2 = (1000 + y_ongtren2) + KhoangcachHaiOng;
+                //x_ong2 = x_ong1 + pcbOngTren1.Width + 400;
+                x_ong2 = 500 + pcbOngTren1.Width + 400;
+                pcbOngTren2.Location = new Point(x_ong2, y_ongtren2);
+                pcbOngDuoi2.Location = new Point(x_ong2, y_ongduoi2);
+            }
+            label1.Text = "Điểm" +  diemso.ToString();
+
+
+
+        }
+
+    }
+}
